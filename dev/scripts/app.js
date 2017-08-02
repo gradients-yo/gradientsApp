8ba023b02eea62bbbafa7abdcdb601286bc98302
Estela <estelathomson@me.com>
2017-08-01 22:07:17 -0400

    user auth

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import firebase, {auth, database, provider, dbRef} from "./firebase.js";
-import CreateGradient from "./components/GradientInputs";
+import firebase, {auth, database, provider, dbRef} from './firebase.js';
+import jQuery from 'jquery';
+import miniColors from '@claviska/jquery-minicolors';
+import CreateGradient from './components/GradientInputs';
 
 class App extends React.Component {
 	constructor() {
@@ -41,12 +43,27 @@ class App extends React.Component {
 	}
 
 	render() {
+		const displayGradients = () => {
+			if (this.state.loggedIn === true) {
+				return (
+					<div>
+						<button onClick={this.logout}>Logout</button>
+						<CreateGradient />
+					</div>
+				)
+			} else {
+				return (
+					<div>
+						<button onClick={this.login}>Login</button>
+					</div>
+				)
+			}
+		}
 		return (
-			<div>
-				<button onClick={this.login}>Login</button>
-				<CreateGradient />
-				<button onClick={this.logout}>Logout</button>
-			</div>
+			<main>
+				{displayGradients()}
+				{colorPicker()}
+			</main>
 		)
 	}
 
@@ -57,6 +74,7 @@ class App extends React.Component {
 					loggedIn: true,
 					user: user
 				})
+
 			} else {
 				this.setState({
 					loggedIn: false,
@@ -64,6 +82,10 @@ class App extends React.Component {
 				})
 			}
 		});
+	}
+
+	colorPicker() {
+		$('INPUT.minicolors').minicolors(settings);
 	}
 }
 
